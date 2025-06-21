70184240bff32034e9a394c091634c776ed5edd7
Vladimir <karasev.vladimir.2000@gmail.com>
2025-06-21 14:00:31 +0300

    Started BinaryHeap implementation

---
+++ lib/data_structures/heap/binary_heap.hpp
@@ -0,0 +1,20 @@
+#pragma once 
+
+// std
+#include <vector>
+
+namespace dsa
+{
+    
+// template <typename T>
+class BinaryHeap
+{
+public:
+    BinaryHeap() = default;
+
+private:
+    // std::vector<T> elements_;
+    std::vector<int> elements_;
+};
+
+} // namespace dsa
